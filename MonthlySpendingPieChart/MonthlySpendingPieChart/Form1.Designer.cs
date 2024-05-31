eb3b174f9945a2ce37bba654c749da2edb000391
SeyfullahAcar <seyfo4242acar@gmail.com>
2024-05-31 16:23:24 +0300

    MonthlySpendingPieChart
    Kullanıcıların aylık kredi kartı harcamalarını grafik formunda görselleştirmek için WinForms ve C# kullanıldı.

---
+++ MonthlySpendingPieChart/MonthlySpendingPieChart/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace MonthlySpendingPieChart
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea8 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend8 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series8 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.gida = new System.Windows.Forms.TextBox();
+            this.akaryakit = new System.Windows.Forms.TextBox();
+            this.egitim = new System.Windows.Forms.TextBox();
+            this.saglik = new System.Windows.Forms.TextBox();
+            this.diger = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(36, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gıda";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(36, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Akaryakıt";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(36, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Eğitim";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(36, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 22);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Sağlık";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(36, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Diğer";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(37, 435);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(200, 18);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Seçilen Harcama Yüzdesi";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(324, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(267, 22);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Aylık Kredi Kartı Harcamaları";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(94, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Hesapla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(206, 384);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Yüzde Hesapla";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Eğitim",
+            "Gıda",
+            "Akaryakıt",
+            "Sağlık",
+            "Diğer"});
+            this.comboBox1.Location = new System.Drawing.Point(40, 389);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 24);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // gida
+            // 
+            this.gida.Location = new System.Drawing.Point(150, 95);
+            this.gida.Name = "gida";
+            this.gida.Size = new System.Drawing.Size(113, 22);
+            this.gida.TabIndex = 10;
+            // 
+            // akaryakit
+            // 
+            this.akaryakit.Location = new System.Drawing.Point(150, 128);
+            this.akaryakit.Name = "akaryakit";
+            this.akaryakit.Size = new System.Drawing.Size(113, 22);
+            this.akaryakit.TabIndex = 11;
+            // 
+            // egitim
+            // 
+            this.egitim.Location = new System.Drawing.Point(150, 164);
+            this.egitim.Name = "egitim";
+            this.egitim.Size = new System.Drawing.Size(113, 22);
+            this.egitim.TabIndex = 12;
+            // 
+            // saglik
+            // 
+            this.saglik.Location = new System.Drawing.Point(150, 202);
+            this.saglik.Name = "saglik";
+            this.saglik.Size = new System.Drawing.Size(113, 22);
+            this.saglik.TabIndex = 13;
+            // 
+            // diger
+            // 
+            this.diger.Location = new System.Drawing.Point(150, 239);
+            this.diger.Name = "diger";
+            this.diger.Size = new System.Drawing.Size(113, 22);
+            this.diger.TabIndex = 14;
+            // 
+            // chart1
+            // 
+            chartArea8.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea8);
+            legend8.Name = "Legend1";
+            this.chart1.Legends.Add(legend8);
+            this.chart1.Location = new System.Drawing.Point(435, 79);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.SemiTransparent;
+            series8.ChartArea = "ChartArea1";
+            series8.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series8.Legend = "Legend1";
+            series8.Name = "s1";
+            this.chart1.Series.Add(series8);
+            this.chart1.Size = new System.Drawing.Size(405, 356);
+            this.chart1.TabIndex = 15;
+            this.chart1.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 509);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.diger);
+            this.Controls.Add(this.saglik);
+            this.Controls.Add(this.egitim);
+            this.Controls.Add(this.akaryakit);
+            this.Controls.Add(this.gida);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox gida;
+        private System.Windows.Forms.TextBox akaryakit;
+        private System.Windows.Forms.TextBox egitim;
+        private System.Windows.Forms.TextBox saglik;
+        private System.Windows.Forms.TextBox diger;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ MonthlySpendingPieChart/MonthlySpendingPieChart/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Xml.Linq;
+
+namespace MonthlySpendingPieChart
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float d1, d2, d3, d4, d5, toplam;
+            d1 = int.Parse(gida.Text);
+            d2 = int.Parse(akaryakit.Text);
+            d3 = int.Parse(egitim.Text);
+            d4 = int.Parse(saglik.Text);
+            d5 = int.Parse(diger.Text);
+
+            toplam = d1 + d2 + d3 + d4 + d5;
+
+            float pd1, pd2, pd3, pd4, pd5;
+            double yd1, yd2, yd3, yd4, yd5;
+            pd1 = (d1 / toplam) * 360;
+            pd2 = (d2 / toplam) * 360;
+            pd3 = (d3 / toplam) * 360;
+            pd4 = (d4 / toplam) * 360;
+            pd5 = (d5 / toplam) * 360;
+
+            yd1 = (d1 / toplam) * 100;
+            yd2 = (d2 / toplam) * 100;
+            yd3 = (d3 / toplam) * 100;
+            yd4 = (d4 / toplam) * 100;
+            yd5 = (d5 / toplam) * 100;
+
+
+            chart1.Titles.Add("Harcamaların Pasta Grafiği");
+            chart1.Series["s1"].Points.AddXY("%" + yd1.ToString("0.0") +"\nGıda",pd1);
+            chart1.Series["s1"].Points.AddXY("%" + yd2.ToString("0.0")+ "\nAkaryakıt",pd2);
+            chart1.Series["s1"].Points.AddXY("%" + yd3.ToString("0.0")+ "\nEğitim", pd3);
+            chart1.Series["s1"].Points.AddXY("%" + yd4.ToString("0.0")+ "\nSağlık", pd4);
+            chart1.Series["s1"].Points.AddXY("%" + yd5.ToString("0.0")+"\nDiğer", pd5);
+
+            chart1.Series["s1"].Points[0].LegendText = "Gıda";
+            chart1.Series["s1"].Points[1].LegendText = "Akaryakıt";
+            chart1.Series["s1"].Points[2].LegendText = "Eğitim";
+            chart1.Series["s1"].Points[3].LegendText = "Sağlık";
+            chart1.Series["s1"].Points[4].LegendText = "Diğer";
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            float d1, d2, d3, d4, d5, toplam;
+            d1 = int.Parse(gida.Text);
+            d2 = int.Parse(akaryakit.Text);
+            d3 = int.Parse(egitim.Text);
+            d4 = int.Parse(saglik.Text);
+            d5 = int.Parse(diger.Text);
+
+            toplam = d1 + d2 + d3 + d4 + d5;
+            double yd1, yd2, yd3, yd4, yd5;
+
+            yd1 = (d1 / toplam) * 100;
+            yd2 = (d2 / toplam) * 100;
+            yd3 = (d3 / toplam) * 100;
+            yd4 = (d4 / toplam) * 100;
+            yd5 = (d5 / toplam) * 100;
+
+            string turuNe = comboBox1.SelectedItem.ToString();
+
+            if (turuNe.Equals("Gıda"))
+            {
+                label6.Text = turuNe + " harcamaları %" + yd1.ToString("0.0") + " oranındadır";
+            }
+            else if (turuNe.Equals("Akaryakıt"))
+            {
+                label6.Text = turuNe + " harcamaları %" + yd2.ToString("0.0") + " oranındadır";
+
+            }
+            else if (turuNe.Equals("Eğitim"))
+            {
+                label6.Text = turuNe + " harcamaları %" + yd3.ToString("0.0") + " oranındadır";
+            }
+            else if (turuNe.Equals("Sağlık"))
+            {
+                label6.Text = turuNe + " harcamaları %" + yd4.ToString("0.0") + " oranındadır";
+            }
+            else if (turuNe.Equals("Diğer"))
+            {
+                label6.Text = turuNe + " harcamalar %" + yd5.ToString("0.0") + " oranındadır";
+            }
+        }
+    }
+}
